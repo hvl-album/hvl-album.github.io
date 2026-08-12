f8800eccc7dcdcdab99e9b243db05d5a225dc5bc
votienthuan97 <votienthuan97@gmail.com>
2026-08-12 13:59:06 +0700

    Add Lyrics Intepol

--- src/components/lyrics/intepol.ts
+++ src/components/lyrics/intepol.ts
@@ -7,12 +7,12 @@ export const intepolLyrics: LyricsTrack = {
 Đứng ở dưới thì nhảy mạnh lên
 Ai sợ thì đi về
 Dân mạng vô cùng bức xúc trước lời chia sẻ của MC-mạng vô-dân mạng vô-vô cùng-vô cùng bức
-Thậm chí, thái độ bị cho là đang thách thức khán-thậm chí-thậm chí-đang-thậm chí (Tin tức, MCK vấp phải nhiều lời chỉ trích-của khán-của [?] đang dành cho nam rapper [?])
-(Đề cập, Phát Thanh Truyền Hình cũng đã đề cập-đề cập-đề cập)
+Thậm chí, thái độ bị cho là đang thách thức khán (thậm chí-thậm chí-đang-thậm chí)
+(MCK vấp phải nhiều lời chỉ trích) (Đề cập, Phát Thanh Truyền Hình cũng đã đề cập-đề cập-đề cập)
 MCK vẫn bị chê nhạc dở
-Phát ngôn, hành động của [?]
+Phát ngôn, hành động của
 K vẫn-K vẫn-K vẫn bị-vẫn bị
-[?] ngông trên sân khấu
+(ngông trên sân khấu)
 Bị tố-sân khấu-sân khấu-bị tố
 Tự dưng nhiều view thế nhở
 Nổi tiếng-nổi tiếng-nhiều vi-nhiều
@@ -20,11 +20,11 @@ Nhiều view-view
 
 That's my mo'fucking ninja, MCK, man
 Top một trending, và được chương trình giải trí với công chúng với-
-(Địt mẹ nó flow đỉnh vãi lồn)
+(Flow đỉnh vãi l*n)
 MCK đã đánh bại HIEUTHUHAI
-(Lồn)
-? M-M-M
-Rapper "Ai sợ thì đi về" [?]
+(đỉnh vãi l*n)
+M-M-M
+Rapper "Ai sợ thì đi về"
 Đã thuộc-(Của ai)-của MCK (thì người đấy có quyền)
 What I'm talking about, G, that shit slap, that shit tough, that shit hard, that shit, ah
 Thì mình có giải thưởng dành cho-
@@ -32,8 +32,8 @@ Thì mình có giải thưởng dành cho-
 99%, MCK
 `,
   lyricsTimestamps: [
-    1.10, 2.54, 3.98, 5.41, 9.22, 16.50, 19.11, 20.55, 21.99, 23.43,
-    24.87, 26.30, 27.74, 29.20, 30.64, 32.18, 35.04, 36.48, 37.92, 39.36,
-    40.80, 42.24, 44.38, 48.19, 49.62, 51.06,
+    1.10, 2.54, 3.98, 5.41, 9.22, 12.50, 16.11, 16.55, 17.2, 18.5,
+    19.4, 22.3, 23.74, 24.20, 27.5, 29.8, 31.7, 33.1, 34.1, 35.2,
+    36.3, 38.2, 41.35, 46.4, 47.8, 49.1,
   ],
 } as const;
